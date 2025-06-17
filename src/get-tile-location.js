3bcbdfd0e96e75582243936f8bdc275b16512e84
Ardhi Lukianto <ardhi@lukianto.com>
2025-06-17 21:20:54 +0700

    Updates

--- src/get-tile-location.js
+++ src/get-tile-location.js
@@ -2,6 +2,7 @@ function getTileLocation ({ type, prefix = '', z, x, y, format = '.png' }) {
   const ry = Math.pow(2, z) - y - 1
   if (type === 'tms') return `${prefix}/${z}/${x}/${ry}${format}`
   if (type === 'rtms') return `${prefix}/${ry}/${x}/${z}${format}`
+  if (type === 'yxz') return `${prefix}/${y}/${x}/${z}${format}`
   return `${prefix}/${z}/${x}/${y}${format}`
 }
 
